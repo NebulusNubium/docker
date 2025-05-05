2ae423ab54154626f2d5220b8c7b36d270aa8f30
Théo <t.knopfer@gmail.com>
2025-05-05 16:36:06 +0200

    exercice 2

---
+++ www/projet1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anima</title>
+</head>
+<body>
+<?php
+    $myvariable = "coucou";
+    ?>
+    <h1><?php echo $myvariable ?></h1>
+    <?php echo "<h1>Mon super titre</h1>" ?>
+    <?php echo "<h1> .$myvariable </h1>" ?>
+    
+
+</body>
+</html>
